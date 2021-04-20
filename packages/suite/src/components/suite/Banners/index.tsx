0b784e6b2ff51e845e9798829021da2e581d4d7b
tomasklima <tomas.klim@gmail.com>
2021-04-20 14:16:53 +0200

    feat(suite): redesign, prioritise banners and add message system banner

--- packages/suite/src/components/suite/Banners/index.tsx
+++ packages/suite/src/components/suite/Banners/index.tsx
@@ -1,26 +1,26 @@
 import * as React from 'react';
 import styled from 'styled-components';
+
+import { Message } from '@suite-types/messageSystem';
 import { isDesktop } from '@suite-utils/env';
 import { useSelector } from '@suite-hooks';
-
 import OnlineStatus from './OnlineStatus';
 import UpdateBridge from './UpdateBridge';
 import UpdateFirmware from './UpdateFirmware';
 import NoBackup from './NoBackup';
 import FailedBackup from './FailedBackup';
+import MessageSystemBanner from './MessageSystemBanner';
 
 const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
     z-index: 3;
+    background: ${props => props.theme.BG_WHITE};
 `;
 
 const Banners = () => {
     const transport = useSelector(state => state.suite.transport);
     const online = useSelector(state => state.suite.online);
     const device = useSelector(state => state.suite.device);
-
-    let Banner;
+    const { validMessages, dismissedMessages, config } = useSelector(state => state.messageSystem);
 
     const showUpdateBridge = () => {
         if (
@@ -33,25 +33,52 @@ const Banners = () => {
         return transport?.outdated;
     };
 
+    const getMessageSystemBanner = (): Message | null => {
+        const nonDismissedValidMessages = validMessages.banner.filter(
+            id => !dismissedMessages[id]?.banner,
+        );
+
+        const messages = config?.actions
+            .filter(({ message }) => nonDismissedValidMessages.includes(message.id))
+            .map(action => action.message);
+
+        if (!messages?.length) return null;
+
+        return messages.reduce((prev, current) =>
+            prev.priority > current.priority ? prev : current,
+        );
+    };
+
+    let banner;
+    let priority = 0;
     if (device?.features?.unfinished_backup) {
-        Banner = <FailedBackup />;
+        banner = <FailedBackup />;
+        priority = 9;
     } else if (device?.features?.needs_backup) {
-        Banner = <NoBackup />;
+        banner = <NoBackup />;
+        priority = 7;
     } else if (showUpdateBridge()) {
-        Banner = <UpdateBridge />;
+        banner = <UpdateBridge />;
+        priority = 5;
     } else if (
         device?.connected &&
         device?.features &&
         device?.mode !== 'bootloader' &&
         ['outdated'].includes(device.firmware)
     ) {
-        Banner = <UpdateFirmware />;
+        banner = <UpdateFirmware />;
+        priority = 3;
+    }
+
+    const messageSystemBanner = getMessageSystemBanner();
+    if (messageSystemBanner && messageSystemBanner.priority >= priority) {
+        banner = <MessageSystemBanner message={messageSystemBanner} />;
     }
 
     return (
         <Wrapper>
             <OnlineStatus isOnline={online} />
-            {Banner}
+            {banner}
             {/* TODO: add Pin not set */}
         </Wrapper>
     );
