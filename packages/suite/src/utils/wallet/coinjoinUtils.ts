996eb3e37ef9605a950ac8ecacf3d235e932ccf2
Szymon Lesisz <sz.lesisz@gmail.com>
2022-09-29 18:28:42 +0200

    chore(suite): sanitize coinjoin account

--- packages/suite/src/utils/wallet/coinjoinUtils.ts
+++ packages/suite/src/utils/wallet/coinjoinUtils.ts
@@ -1,6 +1,7 @@
 import BigNumber from 'bignumber.js';
-import { getUtxoOutpoint } from '@suite-common/wallet-utils';
-import { Account } from '@suite-common/wallet-types';
+import { getUtxoOutpoint, getBip43Type } from '@suite-common/wallet-utils';
+import { Account, CoinjoinSessionParameters } from '@suite-common/wallet-types';
+import { Account as CoinjoinAccount } from '@trezor/coinjoin';
 
 export type CoinjoinBalanceBreakdown = {
     notAnonymized: string;
@@ -54,3 +55,43 @@ export const breakdownCoinjoinBalance = ({
 
     return balanceBreakdown;
 };
+
+// convert suite account type to @trezor/coinjoin (Wasabi) account type
+const getAccountType = (path: string) => {
+    const bip43 = getBip43Type(path);
+    switch (bip43) {
+        case 'bip86':
+        case 'slip25':
+            return 'Taproot';
+        case 'bip84':
+            return 'P2WPKH';
+        default:
+            return 'P2WPKH';
+    }
+};
+
+const getUtxos = (utxos: Account['utxo']) =>
+    utxos
+        ?.filter(utxo => utxo.confirmations) // TODO: filter anonymized and "out of the basket" utxos
+        .map(utxo => ({
+            ...utxo,
+            outpoint: getUtxoOutpoint(utxo),
+            amount: Number(utxo.amount),
+        }));
+
+// transform suite Account to @trezor/coinjoin Account
+// TODO: validate and throw errors (account type, symbol)
+export const sanitizeAccount = (
+    account: Account,
+    params: CoinjoinSessionParameters,
+): CoinjoinAccount => ({
+    type: getAccountType(account.path),
+    symbol: account.symbol as any,
+    descriptor: account.key,
+    anonymityLevel: 0,
+    maxRounds: params.maxRounds,
+    maxFeePerKvbyte: params.maxFeePerKvbyte,
+    maxCoordinatorFeeRate: params.maxCoordinatorFeeRate,
+    utxos: getUtxos(account.utxo) || [],
+    addresses: account.addresses!.change,
+});
