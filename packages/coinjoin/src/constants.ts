14185466a6319f8b3ee25886bf6f800e9b6c9b56
Szymon Lesisz <sz.lesisz@gmail.com>
2022-10-13 17:41:11 +0200

    feat(coinjoin): add Status module
    fetching cooridinator status

---
+++ packages/coinjoin/src/constants.ts
@@ -0,0 +1,5 @@
+export const STATUS_TIMEOUT = {
+    idle: 60000, // no registered accounts, occasionally fetch status to read fees
+    enabled: 30000, // account is registered but utxo was not paired with Round
+    registered: 10000, // utxo is registered in Round
+} as const;
