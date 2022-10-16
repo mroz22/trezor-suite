6e0a9eae2e4e3ccb1cd128f22112dfcb4ef62f3d
Szymon Lesisz <sz.lesisz@gmail.com>
2022-10-16 18:22:40 +0200

    fixup! feat(suite): add debug.coinjoinServerEnvironment flag

--- packages/suite/src/services/coinjoin/coinjoinBackend.ts
+++ packages/suite/src/services/coinjoin/coinjoinBackend.ts
@@ -1,7 +1,7 @@
 import { CoinjoinBackend } from '@trezor/coinjoin';
 import { createIpcProxy } from '@trezor/ipc-proxy';
 import { isDesktop } from '@suite-utils/env';
-import { Account, CoinjoinServerEnvironment } from '@suite-common/wallet-types';
+import { CoinjoinServerEnvironment } from '@suite-common/wallet-types';
 import { COINJOIN_NETWORKS } from './config';
 
 const loadInstance = (network: string, environment?: CoinjoinServerEnvironment) => {
